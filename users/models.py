a0e8cd52211844ebd01e9a3b7c936e953f67ac03
Dominik <domi9132@gmail.com>
2026-05-08 15:55:21 +0200

    feat(users): add Counselor model linked to User

--- users/models.py
+++ users/models.py
@@ -11,3 +11,28 @@ class User(AbstractUser):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+    
+class Counselor(models.Model):
+    """
+    Savjetnik model linkan na User account.
+    """
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name='counselor_profile'
+    )
+
+    class Meta:
+        verbose_name = 'Counselor'
+        verbose_name_plural = 'Counselors'
+
+    def __str__(self):
+        return f"{self.user.first_name} {self.user.last_name}"
+
+    @property
+    def full_name(self):
+        return f"{self.user.first_name} {self.user.last_name}"
+
+    @property
+    def email(self):
+        return self.user.email
