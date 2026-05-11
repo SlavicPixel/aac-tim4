9c521bc82ec7ae7e5c294013ce33619d7719cdcd
Dominik <domi9132@gmail.com>
2026-05-11 18:30:26 +0200

    feat(users): add login system with role-based dashboards

--- aac_tim4/settings.py
+++ aac_tim4/settings.py
@@ -138,3 +138,7 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 AUTH_USER_MODEL = 'users.User'
+
+LOGIN_URL = 'users:login'
+LOGIN_REDIRECT_URL = 'core:dashboard'
+LOGOUT_REDIRECT_URL = 'users:login'
